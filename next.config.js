9e1cc2415bd133cf05a3aa138e08730886dabff6
YasserFarahat <brooklynlectures@gmail.com>
2026-05-27 10:46:51 +0300

    Add Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  },
+};
+
+module.exports = nextConfig;
