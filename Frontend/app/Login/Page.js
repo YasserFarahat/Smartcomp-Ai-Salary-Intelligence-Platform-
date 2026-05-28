d6bfd055d20421b5b4d2033b9d4746111fa95f00
YasserFarahat <brooklynlectures@gmail.com>
2026-05-28 04:48:38 +0300

    Create Page.js

---
+++ Frontend/app/Login/Page.js
@@ -0,0 +1,19 @@
+export default function LoginPage() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        background: "#0f172a",
+        color: "white"
+      }}
+    >
+      <h1>SmartComp AI Login</h1>
+
+      <p>Salary Intelligence Platform</p>
+    </main>
+  );
+}
